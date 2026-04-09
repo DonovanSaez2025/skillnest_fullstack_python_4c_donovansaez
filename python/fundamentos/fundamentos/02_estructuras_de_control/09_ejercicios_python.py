f5f2a2767baccd01f60e9f170277e6946484aa63
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-04-09 09:03:09 -0400

    d
    d

---
+++ python/fundamentos/fundamentos/02_estructuras_de_control/09_ejercicios_python.py
@@ -0,0 +1,3 @@
+# Ejercicio 1: Números Pares Dinámicos
+'''Desarrolla un programa que solicite al usuario cuántos números pares desea ver ($n$). 
+El programa debe imprimir los primeros $n$ números pares positivos.'''
