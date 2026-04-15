2ef59125985fc61e8d436e88382046e5a4e21b17
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-04-15 12:57:23 -0400

    b
    vb

--- python/fundamentos/fundamentos/Bucles_for_básico_1_(Core)_Donovan_2026-04-14/bucle_for_basico1.py
+++ python/fundamentos/fundamentos/Bucles_for_básico_1_(Core)_Donovan_2026-04-14/bucle_for_basico1.py
@@ -3,8 +3,23 @@ def nivelesImprimir():
     for i in range(101):
         print(f"Nivel {i}")
     
-#
+# Encontrar potenciadores cada 2 pasos
 def potenciadorEncontrar():
     for i in range(2, 501):
         if i % 2 == 0:
             print(f"Potenciador encontrado en la posición {i}")
+            
+# Trampa de emojis en pasos que sean divisibles por 10 o en 5
+def trampaEmoji():
+    for i in range(1, 101):
+        if i % 10 == 0:
+            print(f"Punto {i} maloso") #cambiar por emoji
+        elif i % 5 == 0:
+            print(f"Punto {i} Divertido") #cambiar por emoji
+
+#
+bonusFinal = 0
+for i in range(1, 500000):
+    if i % 2 == 0:
+        bonusFinal += i
+print(f"La recompensa final es de {bonusFinal} puntos")
