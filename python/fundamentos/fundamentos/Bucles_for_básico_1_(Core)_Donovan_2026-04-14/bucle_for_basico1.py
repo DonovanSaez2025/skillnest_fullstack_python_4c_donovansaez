deeee185827c953bb68914119a83516e70cff82e
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-04-15 15:17:03 -0400

    df
    df

--- python/fundamentos/fundamentos/Bucles_for_básico_1_(Core)_Donovan_2026-04-14/bucle_for_basico1.py
+++ python/fundamentos/fundamentos/Bucles_for_básico_1_(Core)_Donovan_2026-04-14/bucle_for_basico1.py
@@ -17,9 +17,26 @@ def trampaEmoji():
         elif i % 5 == 0:
             print(f"Punto {i} Divertido") #cambiar por emoji
 
-#
-bonusXP = 0
-for i in range(1, 500001):
-    if i % 2 == 0:
-        bonusXP += i
-print(f"La recompensa final es de {bonusXP} puntos")
+# Sumatoria de bonus XP en segundos pares hasta pasados 500000 segundos
+def sumarBonus():
+    bonusXP = 0
+    for i in range(1, 500001):
+        if i % 2 == 0:
+            bonusXP += 1
+    print(f"La recompensa final es de {bonusXP} puntos")
+
+# Viajar en el tiempo al pasado hasta el año 0 o menos
+def viajeTemporal():
+    anioActual = 2024
+    while anioActual > 0:
+        anioActual -= 3
+        print(f"El año actual es {anioActual}")
+
+# Contador de niveles dinámico con intervalos de salto
+def contadorDinamico():
+    inicio = int(input("Ingresa un número de inicio: "))
+    fin = int(input("Ingresa un número final: "))
+    salto = int(input("Ingresa un salto de intervalos: "))
+    for i in range(inicio, fin + 1):
+        if i % salto == 0:
+            print(f"El nivel actual es {i}")
