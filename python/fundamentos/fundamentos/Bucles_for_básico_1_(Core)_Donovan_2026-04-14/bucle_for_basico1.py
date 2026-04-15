a7a0ec3a9c93bb52a836ca4a077bda22bae02ac6
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-04-15 11:58:27 -0400

    fg
    dfgdfg

---
+++ python/fundamentos/fundamentos/Bucles_for_básico_1_(Core)_Donovan_2026-04-14/bucle_for_basico1.py
@@ -0,0 +1,10 @@
+# Niveles del 0 al 100
+def nivelesImprimir():
+    for i in range(101):
+        print(f"Nivel {i}")
+    
+#
+def potenciadorEncontrar():
+    for i in range(2, 501):
+        if i % 2 == 0:
+            print(f"Potenciador encontrado en la posición {i}")
