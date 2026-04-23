dedf027b16eeb274f26d3277b25c90da1ea821d8
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-04-23 10:26:04 -0400

    d
    d

--- python/Core/Funciones_intermedias_(Core)_DonovanSaez_2026-04-23/funciones_intermedias.py
+++ python/Core/Funciones_intermedias_(Core)_DonovanSaez_2026-04-23/funciones_intermedias.py
@@ -1,3 +1,5 @@
+'''Aclaración: .items() rompe con el formato que nos pide skillnest para las salidas de datos,
+así que solo fue usado en el bonus 3 ya que no nos daba formato que .items() rompía'''
 # Ranking de puntajes de un torneo de eSports
 puntajes = [ [1000, 1500, 2000], [300, 700, 1400] ]
 
@@ -37,14 +39,58 @@ print(eventos["Estados Unidos"][2])
 # 4. En ubicacion, cambia el valo de ”latitud” a 40.712776 (cambiando la sede del torneo a Nueva York).
 ubicacion[0]["latitud"] = 40.712776
 print(ubicacion[0]["latitud"])
-# Ejercicios de recorrer datos
 
-'''Crea la función iterar_diccionario(lista) que reciba una lista de diccionarios (como streamers)
+# Ejercicios de recorrer datos
+'''1. Crea la función iterar_diccionario(lista) que reciba una lista de diccionarios (como streamers)
 y recorra cada uno, imprimiendo sus claves y valores.'''
-def iterar_diccionario(listaStreamers):
-    print("\nImprimiendo lista de streamers:")
-    for i in range(0, len(listaStreamers)):
-        print(f"nombre - {listaStreamers[i]["nombre"]}, seguidores - {listaStreamers[i]["seguidores"]}")
-        
-            
+def iterar_diccionario(lista):
+    print("\nImprimiendo ejercicio 1:")
+    for i in range(0, len(lista)):
+        print(f"nombre - {lista[i]["nombre"]}, seguidores - {lista[i]["seguidores"]}")
 iterar_diccionario(streamers)
+
+'''2. Obtener valores de un diccionario creando la función obtener_valores(clave, lista) que reciba,
+por una parte, una cadena con el nombre de una clave, por otra, una lista de diccionarios.
+La función debe imprimir el valor asociado a esa clave en cada uno de los diccionarios.'''
+def obtener_valores(clave, lista):
+    print("\nImprimiendo ejercicio 2:")
+    for i in range(0, len(lista)):
+        print(streamers[i][clave])
+    print("")
+obtener_valores("nombre", streamers)
+obtener_valores("seguidores", streamers)
+
+'''3. Crea la función mostrar_informacion(diccionario), que reciba un diccionario en el que
+los valores sean listas. La función debe, por una parte, imprimir el tamaño de la lista
+y la clave en mayúsculas, por otra, imprimir cada elemento de la lista en líneas separadas.'''
+def mostrar_informacion(diccionario):
+    print("\nImprimiendo ejercicio 3:")
+    print(f"{len(diccionario["Estados Unidos"])} CIUDADES_EVENTOS")
+    for i in range(0, len(diccionario["Estados Unidos"])):
+        print(diccionario["Estados Unidos"][i])
+mostrar_informacion(eventos)
+
+# Desafíos extras
+datos = [
+    {"nombre": "Carlos", "puntaje": 80},
+    {"nombre": "María", "puntaje": 95},
+    {"nombre": "Pedro", "puntaje": 70}
+]
+
+# 1. Cambiar el puntaje de Pedro a 75
+datos[2]["puntaje"] = 75
+print(datos[2]["puntaje"])
+
+# 2. Crear función que imprima: "Carlos obtuvo 80 puntos"
+def puntajeObtenido(index, lista):
+    print("\nImprimiendo ejercicio bonus 2:")
+    print(f"{lista[index]["nombre"]} obtuvo {lista[index]["puntaje"]} puntos")
+puntajeObtenido(0, datos)
+
+# 3. Crear función que reciba "nombre" o "puntaje" e imprima solo esos valores
+def nombrePuntaje(lista):
+    print("\nImprimiendo ejercicio bonus 3:")
+    for i in range(0, len(lista)):
+        for clave, valor in lista[i].items():
+            print(f"{valor}")
+nombrePuntaje(datos)
