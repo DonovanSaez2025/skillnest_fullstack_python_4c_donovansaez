61e33b33c934bc1493a8870170911d0ba5e167ae
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-04-30 09:29:28 -0400

    v
    v

--- python/fundamentos/extras/saez_donovan_funciones.py
+++ python/fundamentos/extras/saez_donovan_funciones.py
@@ -27,7 +27,10 @@ def contarVocales(texto):
         
 def ejercicio2():
     texto = input("Ingresa un texto: ").strip()
-    contarVocales(texto.lower())
+    if texto == "":
+        print("Error: no puede estar vacío.")
+    else:
+        contarVocales(texto.lower())
 
 # 3. Crear una función que reciba una lista de nombres
 # y muestre únicamente aquellos que tengan más de 5 letras.
@@ -41,9 +44,14 @@ def mostrarNombres(listaNombres):
 def ejercicio3():
     limit = int(input("Ingresa el límite de valores: "))
     listaNombres = []
-    for i in range(0, limit):
-        nombre = input("Ingresa un nombre: ")
-        listaNombres.append(nombre)
+    i = 0
+    while i < limit:
+        nombre = input("Ingresa un nombre: ").strip()
+        if nombre == "":
+            print("Error: no puede estar vacío.")
+        else:
+            listaNombres.append(nombre)
+            i+=1
     mostrarNombres(listaNombres)
 
 # 4. Crear una función que reciba una lista de notas (números decimales),
@@ -59,18 +67,15 @@ def promedioNotas(listaNotas):
 def ejercicio4():
     limit = int(input("Ingresa el límite de notas: "))
     listaNotas = []
-    go = False
-    for i in range(0, limit):
+    i = 0
+    while i < limit:
         nota = float(input("Ingresa una nota del estudiante: "))
         if nota > 7.0 or nota < 1.0:
             print("Error: las notas no pueden ser mayores a 7.0 y menores a 1.0")
-            go = False
-            break
         else:
             listaNotas.append(nota)
-            go = True
-    if go == True:
-        promedioNotas(listaNotas)
+            i+=1
+    promedioNotas(listaNotas)
 
 # 5. Crear una función que reciba una lista de precios de productos
 # y aplique un descuento del 10%, mostrando el valor original y el nuevo valor.
@@ -107,39 +112,71 @@ def mayoresEdad(listaEdades):
     for i in range(0, len(listaEdades)):
         if listaEdades[i] >= 18:
             mayorEdad += 1
-    print(f"{mayorEdad} personas son mayores de edad.")
+    if mayorEdad == 0:
+        print(f"No hay personas mayores de edad.")
+    elif mayorEdad == 1:
+        print(f"Solo hay {mayorEdad} persona mayor de edad.")
+    elif mayorEdad > 1:
+        print(f"hay {mayorEdad} personas mayores de edad.")
 
 def ejercicio7():
     limit = int(input("Ingresa el límite de valores: "))
     listaEdades = []
-    go = False
-    for i in range(0, limit):
-        edad = float(input("Ingresa la edad de una persona: "))
-        if edad < 0:
+    i = 0
+    while i < limit:
+        edad = int(input("Ingresa la edad de una persona: "))
+        if edad < 0 or edad > 123:
             print("Edad imposible.")
-            go = False
-            break
         else:
             listaEdades.append(edad)
-            go = True
-    if go == True:
-        mayoresEdad(listaEdades)
+            i+=1
+    mayoresEdad(listaEdades)
 
 # 8. Crear una función que reciba una lista de palabras
 # y permita buscar cuántas veces aparece una palabra específica ingresada por el usuario.
 def palabrasRepetidas(listaPalabras):
-    pass
+    palabraBuscar = input("Ingresa una palabra para buscar en la lista de palabras: ")
+    repeticion = 0
+    for i in range(0, len(listaPalabras)):
+        if listaPalabras[i] == palabraBuscar:
+            repeticion += 1
+    if repeticion == 0:
+        print(f"La palabra '{palabraBuscar}' no está en la lista.")
+    elif repeticion == 1:
+        print(f"La palabra '{palabraBuscar}' aparece {repeticion} vez en la lista.")
+    elif repeticion > 1:
+        print(f"La palabra '{palabraBuscar}' aparece {repeticion} veces en la lista.")
 
 def ejercicio8():
-    pass
+    limit = int(input("Ingresa el límite de palabras: "))
+    listaPalabras = []
+    i = 0
+    while i < limit:
+        palabra = input("Ingresa una palabra: ").strip()
+        if palabra == "":
+            print("Error: no puede estar vacío.")
+        else:
+            listaPalabras.append(palabra.lower())
+            i+=1
+    palabrasRepetidas(listaPalabras)
 
 # 9. Crear una función que reciba una lista de números
 # y genere una nueva lista que contenga únicamente los números positivos.
-def numerosPositivos(listaNumeros):
-    pass
+def numerosPositivos(listaNum):
+    numPos = []
+    for i in range(0, len(listaNum)):
+        if listaNum[i] > 0:
+            numPos.append(str(listaNum[i]))
+    print(f"Números positivos en la lista: {" | ".join(numPos)}")
 
 def ejercicio9():
-    pass
+    limit = int(input("Ingresa el límite de valores: "))
+    listaNum = []
+    for i in range(0, limit):
+        num = float(input("Ingresa un número entero positivo o negativo: "))
+        listaNum.append(num)
+    numerosPositivos(listaNum)
+        
 
 # 10. Crear una función que reciba una lista de productos
 # (utilizando diccionarios con nombre y stock) y muestre cuáles tienen un stock menor a 5 unidades.
@@ -147,7 +184,19 @@ def stockMenor(listaProductos):
     pass
 
 def ejercicio10():
-    pass
+    limit = int(input("Ingresa un límite de productos: "))
+    listaProductos = {"nombres": [], "stock": []}
+    i = 0
+    while i < limit:
+        nombreProd = input("Ingresa el nombre de un producto: ").strip()
+        stockProd = int(input("Ingresa el stock de ese producto: "))
+        if nombreProd == "" or stockProd < 0:
+                print("Error: valores inválidos.")
+        else:
+            listaProductos["nombres"].append(nombreProd)
+            listaProductos["stock"].append(stockProd)
+            i += 1
+    stockMenor(listaProductos)
 
 # Limpiar consola
 def limpiarConsola():
@@ -167,7 +216,7 @@ while Continue:
     print("--- Ejercicio 8 ---")
     print("--- Ejercicio 9 ---")
     print("--- Ejercicio 10 ---")
-    opcion = input("Selecciona un ejercicio (1-10): ")
+    opcion = input("Selecciona un ejercicio (1-10): ").strip()
     
     if opcion == "1":
         limpiarConsola()
