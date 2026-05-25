707bf663cd3d12f57eb4b73d6d401bec84d223cb
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-05-25 10:58:11 -0400

    c
    c

--- python/fundamentos/extras/Actividad_grupal_python+sql/sistema_tienda.py
+++ python/fundamentos/extras/Actividad_grupal_python+sql/sistema_tienda.py
@@ -1,10 +1,10 @@
 class Producto:
     tienda = "Porky's Big Shots"
     
-    def __innit__(self, nombre, stock, precio):
-        self.nombre = nombre
-        self.stock = stock
-        self.precio = precio
+    def __innit__(self, nombre_producto, stock_producto, precio_producto):
+        self.nombre_producto = nombre_producto
+        self.stock_producto = stock_producto
+        self.precio_producto = precio_producto
         
     def mostrar_producto(self):
         pass
@@ -16,14 +16,35 @@ class Cliente:
     tienda = "Porky's Big Shots"
     banco = "Bancoestado"
     
-    def __innit__(self, nombre, direccion):
-        self.nombre = nombre
-        self.direccion = direccion
+    def __innit__(self, nombre_cliente, direccion_cliente):
+        self.nombre_cliente = nombre_cliente
+        self.direccion_cliente = direccion_cliente
         self.saldo_prendiente = 0
+    
+    def realizar_compra(self, nombres_productos):
+        pass
+    
+    def pagar_saldo(self, monto):
+        pass
+    
+    def actualizar_direccion(self):
+        pass
+    
+    def mostrar_cliente(self):
         pass
 
 class Venta:
-    def __innit__(self, nombre_producto, nombre_cliente, fecha_venta):
-        self.nombre_producto = nombre_producto
+    tienda = "Porky's Big Shots"
+    banco = "Bancoestado"
+    
+    def __innit__(self, nombre_productos, nombre_cliente, fecha_venta, precio_total):
+        self.nombre_productos = nombre_productos
         self.nombre_cliente = nombre_cliente
         self.fecha_venta = fecha_venta
+        self.precio_total = precio_total
+        
+    def registrar_venta(self):
+        pass
+    
+    def calcular_total(self):
+        pass
