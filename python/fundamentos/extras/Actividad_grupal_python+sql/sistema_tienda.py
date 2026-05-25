eefa175ab4c9cb0bc22bf4b606a72414c28fb603
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-05-25 13:00:24 -0400

    Add product/customer lists and purchase features
    Add global lists for products and clients and append entries in constructors; implement Producto.mostrar_producto and actualizar_stock. Implement Cliente.realizar_compra to handle purchases for specific products, decrement stock, accumulate cliente.saldo_prendiente and create Venta instances. Update Venta to include quantity. Adjust prod01 price and expand the interactive menu with options to display products, update stock, and perform purchases with input validation and stock checks.

--- python/fundamentos/extras/Actividad_grupal_python+sql/sistema_tienda.py
+++ python/fundamentos/extras/Actividad_grupal_python+sql/sistema_tienda.py
@@ -1,30 +1,70 @@
 import os
 class Producto:
     tienda = "Porky's Big Shots"
+    listaProductos = []
     
     def __init__(self, nombre_producto, stock_producto, precio_producto):
         self.nombre_producto = nombre_producto
         self.stock_producto = stock_producto
         self.precio_producto = precio_producto
+        Producto.listaProductos.append(nombre_producto)
         
     def mostrar_producto(self):
-        pass
+        print(f"\nMostrando información del producto '{self.nombre_producto}'")
+        print(f"Nombre del producto: {self.nombre_producto}")
+        print(f"Stock del producto: {self.stock_producto}")
+        print(f"Precio unitario del producto: {self.precio_producto}\n")
     
-    def actualizar_stock(self):
-        pass
+    def actualizar_stock(self, cantidad):
+        print("Actualizando stock del producto...")
+        self.stock_producto += cantidad
+        print(f"Nuevo stock de {self.nombre_producto}: {self.stock_producto}\n")
 
 class Cliente:
     tienda = "Porky's Big Shots"
     banco = "Bancoestado"
+    listaClientes = []
     
     def __init__(self, nombre_cliente, direccion_cliente):
         self.nombre_cliente = nombre_cliente
         self.direccion_cliente = direccion_cliente
         self.saldo_prendiente = 0
+        Cliente.listaClientes.append(self.nombre_cliente)
     
-    def realizar_compra(self, nombres_productos):
-        pass
-    
+    def realizar_compra(self, nombre_producto, cantidad):
+        precio_final = 0.0
+        if nombre_producto == "Laptop":
+            print(f"\nComprando {cantidad} {nombre_producto}/s...")
+            prod01.stock_producto -= cantidad
+            precio_final += prod01.precio_producto * cantidad
+            self.saldo_prendiente += precio_final
+            print(f"Precio final: {precio_final}\nSaldo a pagar total: {self.saldo_prendiente}\n")
+        elif nombre_producto == "Mouse":
+            print(f"\nComprando {cantidad} {nombre_producto}/s...")
+            prod02.stock_producto -= cantidad
+            precio_final += prod02.precio_producto * cantidad
+            self.saldo_prendiente += precio_final
+            print(f"Precio final: {precio_final}\nSaldo a pagar total: {self.saldo_prendiente}\n")
+        elif nombre_producto == "Teclado":
+            print(f"\nComprando {cantidad} {nombre_producto}/s...")
+            prod03.stock_producto -= cantidad
+            precio_final += prod03.precio_producto * cantidad
+            self.saldo_prendiente += precio_final
+            print(f"Precio final: {precio_final}\nSaldo a pagar total: {self.saldo_prendiente}\n")
+        elif nombre_producto == "Parlantes":
+            print(f"\nComprando {cantidad} {nombre_producto}/s...")
+            prod04.stock_producto -= cantidad
+            precio_final += prod04.precio_producto * cantidad
+            self.saldo_prendiente += precio_final
+            print(f"Precio final: {precio_final}\nSaldo a pagar total: {self.saldo_prendiente}\n")
+        elif nombre_producto == "Disco duro externo 2tb":
+            print(f"\nComprando {cantidad} {nombre_producto}/s...")
+            prod05.stock_producto -= cantidad
+            precio_final += prod05.precio_producto * cantidad
+            self.saldo_prendiente += precio_final
+            print(f"Precio final: {precio_final}\nSaldo a pagar total: {self.saldo_prendiente}\n")
+        venta_hecha = Venta(nombre_producto, cantidad, self.nombre_cliente, "25/05/2026", precio_final)
+        
     def pagar_saldo(self, monto):
         pass
     
@@ -38,8 +78,9 @@ class Venta:
     tienda = "Porky's Big Shots"
     banco = "Bancoestado"
     
-    def __init__(self, nombre_productos, nombre_cliente, fecha_venta, precio_total):
-        self.nombre_productos = nombre_productos
+    def __init__(self, nombre_producto, cantidad, nombre_cliente, fecha_venta, precio_total):
+        self.nombre_productos = nombre_producto
+        self.cantidad = cantidad
         self.nombre_cliente = nombre_cliente
         self.fecha_venta = fecha_venta
         self.precio_total = precio_total
@@ -52,7 +93,7 @@ class Venta:
     
 # Crear instancias
 #Productos
-prod01 = Producto("Laptop", 10, 199.90)
+prod01 = Producto("Laptop", 10, 199.99)
 prod02 = Producto("Mouse", 5, 15.99)
 prod03 = Producto("Teclado", 20, 37.99)
 prod04 = Producto("Parlantes", 50, 49.99)
@@ -72,13 +113,256 @@ def limpiarConsola():
 # Menu while
 while True:
     print("--- Actividad grupal sistema_tiendas ---")
-    print("--- 1: mostrar producto ---")
+    print("--- 1: Mostrar producto ---")
+    print("--- 2: Actualizar stock de un producto ---")
+    print("--- 3: Comprar productos ---")
     
     opcion = input("Ingresa una opción: ")
     
     if opcion == "1":
         limpiarConsola()
+        print(f"Productos:\n- {"\n- ".join(Producto.listaProductos)}")
+        ins = input("\nIngresa el nombre exacto de un producto: ")
         
-    if opcion == "0":
+        if ins == "Laptop":
+            prod01.mostrar_producto()
+        elif ins == "Mouse":
+            prod02.mostrar_producto()
+        elif ins == "Teclado":
+            prod03.mostrar_producto()
+        elif ins == "Parlantes":
+            prod04.mostrar_producto()
+        elif ins == "Disco duro externo 2tb":
+            prod05.mostrar_producto()
+        else:
+            print("Producto inválido.\n")
+    elif opcion == "2":
+        limpiarConsola()
+        print(f"Productos:\n- {"\n- ".join(Producto.listaProductos)}")
+        ins = input("\nIngresa el nombre exacto de un producto: ")
+        if ins == "Laptop":
+            cantidad = int(input("Ingresa la cantidad de stock a sumar: "))
+            if cantidad > 0:
+                prod01.actualizar_stock(cantidad)
+            else:
+                print("Inválido.")
+        elif ins == "Mouse":
+            cantidad = int(input("Ingresa la cantidad de stock a sumar: "))
+            if cantidad > 0:
+                prod02.actualizar_stock(cantidad)
+            else:
+                print("Inválido.")
+        elif ins == "Teclado":
+            cantidad = int(input("Ingresa la cantidad de stock a sumar: "))
+            if cantidad > 0:
+                prod03.actualizar_stock(cantidad)
+            else:
+                print("Inválido.")
+        elif ins == "Parlantes":
+            cantidad = int(input("Ingresa la cantidad de stock a sumar: "))
+            if cantidad > 0:
+                prod04.actualizar_stock(cantidad)
+            else:
+                print("Inválido.")
+        elif ins == "Disco duro externo 2tb":
+            cantidad = int(input("Ingresa la cantidad de stock a sumar: "))
+            if cantidad > 0:
+                prod05.actualizar_stock(cantidad)
+            else:
+                print("Inválido.\n")
+        else:
+            print("Producto inválido.\n")
+    elif opcion == "3":
+        limpiarConsola()
+        print(f"Clientes:\n- {"\n- ".join(Cliente.listaClientes)}")
+        ins = input("Ingresa tu nombre: ")
+        if ins == "Donovan":
+            print("\nProductos:")
+            print(f"{prod01.nombre_producto} - Stock: {prod01.stock_producto}")
+            print(f"{prod02.nombre_producto} - Stock: {prod02.stock_producto}")
+            print(f"{prod03.nombre_producto} - Stock: {prod03.stock_producto}")
+            print(f"{prod04.nombre_producto} - Stock: {prod04.stock_producto}")
+            print(f"{prod05.nombre_producto} - Stock: {prod05.stock_producto}")
+            nombre_prod = input("Ingresa el nombre del producto: ")
+            if nombre_prod in Producto.listaProductos:
+                cantidad = int(input("Ingresa la cantidad que quieras de ese producto: "))
+                if nombre_prod == "Laptop":
+                    if prod01.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie01.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Mouse":
+                    if prod02.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie01.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Teclado":
+                    if prod03.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie01.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Parlantes":
+                    if prod04.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie01.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Disco duro externo 2tb":
+                    if prod05.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie01.realizar_compra(nombre_prod, cantidad)
+            else:
+                print("Producto inválido.")
+        if ins == "MatildeX":
+            print("\nProductos:")
+            print(f"{prod01.nombre_producto} - Stock: {prod01.stock_producto}")
+            print(f"{prod02.nombre_producto} - Stock: {prod02.stock_producto}")
+            print(f"{prod03.nombre_producto} - Stock: {prod03.stock_producto}")
+            print(f"{prod04.nombre_producto} - Stock: {prod04.stock_producto}")
+            print(f"{prod05.nombre_producto} - Stock: {prod05.stock_producto}")
+            nombre_prod = input("Ingresa el nombre del producto: ")
+            if nombre_prod in Producto.listaProductos:
+                cantidad = int(input("Ingresa la cantidad que quieras de ese producto: "))
+                if nombre_prod == "Laptop":
+                    if prod01.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie02.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Mouse":
+                    if prod02.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie02.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Teclado":
+                    if prod03.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie02.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Parlantes":
+                    if prod04.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie02.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Disco duro externo 2tb":
+                    if prod05.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie02.realizar_compra(nombre_prod, cantidad)
+            else:
+                print("Producto inválido.")
+        if ins == "Matías":
+            print("\nProductos:")
+            print(f"{prod01.nombre_producto} - Stock: {prod01.stock_producto}")
+            print(f"{prod02.nombre_producto} - Stock: {prod02.stock_producto}")
+            print(f"{prod03.nombre_producto} - Stock: {prod03.stock_producto}")
+            print(f"{prod04.nombre_producto} - Stock: {prod04.stock_producto}")
+            print(f"{prod05.nombre_producto} - Stock: {prod05.stock_producto}")
+            nombre_prod = input("Ingresa el nombre del producto: ")
+            if nombre_prod in Producto.listaProductos:
+                cantidad = int(input("Ingresa la cantidad que quieras de ese producto: "))
+                if nombre_prod == "Laptop":
+                    if prod01.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie03.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Mouse":
+                    if prod02.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie03.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Teclado":
+                    if prod03.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie03.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Parlantes":
+                    if prod04.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie03.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Disco duro externo 2tb":
+                    if prod05.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie03.realizar_compra(nombre_prod, cantidad)
+            else:
+                print("Producto inválido.")
+        if ins == "Randy":
+            print("\nProductos:")
+            print(f"{prod01.nombre_producto} - Stock: {prod01.stock_producto}")
+            print(f"{prod02.nombre_producto} - Stock: {prod02.stock_producto}")
+            print(f"{prod03.nombre_producto} - Stock: {prod03.stock_producto}")
+            print(f"{prod04.nombre_producto} - Stock: {prod04.stock_producto}")
+            print(f"{prod05.nombre_producto} - Stock: {prod05.stock_producto}")
+            nombre_prod = input("Ingresa el nombre del producto: ")
+            if nombre_prod in Producto.listaProductos:
+                cantidad = int(input("Ingresa la cantidad que quieras de ese producto: "))
+                if nombre_prod == "Laptop":
+                    if prod01.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie04.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Mouse":
+                    if prod02.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie04.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Teclado":
+                    if prod03.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie04.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Parlantes":
+                    if prod04.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie04.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Disco duro externo 2tb":
+                    if prod05.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie05.realizar_compra(nombre_prod, cantidad)
+            else:
+                print("Producto inválido.")
+        if ins == "Martín":
+            print("\nProductos:")
+            print(f"{prod01.nombre_producto} - Stock: {prod01.stock_producto}")
+            print(f"{prod02.nombre_producto} - Stock: {prod02.stock_producto}")
+            print(f"{prod03.nombre_producto} - Stock: {prod03.stock_producto}")
+            print(f"{prod04.nombre_producto} - Stock: {prod04.stock_producto}")
+            print(f"{prod05.nombre_producto} - Stock: {prod05.stock_producto}")
+            nombre_prod = input("Ingresa el nombre del producto: ")
+            if nombre_prod in Producto.listaProductos:
+                cantidad = int(input("Ingresa la cantidad que quieras de ese producto: "))
+                if nombre_prod == "Laptop":
+                    if prod01.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie05.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Mouse":
+                    if prod02.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie05.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Teclado":
+                    if prod03.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie05.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Parlantes":
+                    if prod04.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie05.realizar_compra(nombre_prod, cantidad)
+                elif nombre_prod == "Disco duro externo 2tb":
+                    if prod05.stock_producto < cantidad:
+                        print("No hay suficiente stock.")
+                    else:
+                        clie05.realizar_compra(nombre_prod, cantidad)
+            else:
+                print("Producto inválido.")
+    elif opcion == "0":
         limpiarConsola()
         break
+    else:
+        print("Opción inválida.\n")
