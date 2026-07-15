dbb760697117b132dd6eb72d2094d16aee79c409
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-07-15 12:55:57 -0400

    v

---
+++ python/flask/fundamentos/8- Ranking_jugadores_core/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Datos de jugadores con puntajes
+jugadores = [
+    {"nombre": "AlexGamer", "puntaje": 5000},
+    {"nombre": "PixelMaster", "puntaje": 7500},
+    {"nombre": "ShadowNinja", "puntaje": 8200},
+    {"nombre": "CyberWarrior", "puntaje": 9100},
+    {"nombre": "UltraNoob", "puntaje": 3000}
+]
+
+
+# Ruta para mostrar el ranking de jugadores
+@app.route("/ranking")
+def raking():
+    return render_template("ranking.html", jugadores=jugadores)
+# Ruta para mostrar un número limitado de jugadores
+@app.route("/ranking/<int:numero>")
+def limit(numero):
+    return render_template("rankinglimit.html", jugadores=jugadores, numero=numero+1, limit=True)
+
+# Ruta para personalizar el color del ranking
+
+# Ejecutar el servidor
+if __name__ == "__main__":
+    app.run(debug=True)
