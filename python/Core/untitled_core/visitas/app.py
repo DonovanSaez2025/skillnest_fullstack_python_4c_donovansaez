360122ae5aab5638042b1c7e69cf3121c5b38b68
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-09-01 09:30:13 -0400

    b

--- python/Core/untitled_core/visitas/app.py
+++ python/Core/untitled_core/visitas/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = "llave"
 
 @app.route("/")
 def mainRoot():
-    return render_template("index.html")
+    if "contador" not in session:
+        session["contador"] = 0
+        
+    session["contador"] += 1
+    
+    return render_template("index.html", counter=session["contador"])
 
 if __name__ == "__main__":
     app.run(debug=True)
