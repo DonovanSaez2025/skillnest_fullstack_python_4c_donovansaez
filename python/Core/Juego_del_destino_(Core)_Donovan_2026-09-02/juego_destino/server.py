28d7f6e6aecc6a09988220f49db511d828500fe4
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-09-03 08:18:57 -0400

    d

--- python/Core/Juego_del_destino_(Core)_Donovan_2026-09-02/juego_destino/server.py
+++ python/Core/Juego_del_destino_(Core)_Donovan_2026-09-02/juego_destino/server.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 
 # Clave para manejar sesiones en Flask
 app.secret_key = "clave_secreta"
 
 # Ruta principal que muestra el formulario para ingresar datos
-
+@app.route("/")
+def main():
+    return render_template("index.html")
 
 # Ruta para procesar los datos del formulario y almacenarlos en sesión
+@app.route("/enviar", methods=["POST"])
+def procesar():
+    return redirect(url_for("main"))
 
 # Ruta para mostrar la predicción del futuro basada en los datos ingresados
+@app.route("/futuro", methods=["GET"])
+def prediccion():
+    return render_template("futuro.html")
 
+# Debug
 if __name__ == "__main__":
     app.run(debug=True)
